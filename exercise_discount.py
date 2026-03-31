c7258f45767fb72df23921af6ca5ba51253a8f60
Indianasol <ideirmendjian@mail.austral.edu.ar>
2026-03-31 10:19:34 -0300

    Add files via upload

--- exercise_discount.py
+++ exercise_discount.py
@@ -1,27 +1,28 @@
 def discount():
-    """
-    Ejercicio 9 (Integrador) - Sistema de Descuentos
+    precio_unitario= float(input())
+    cant_unidades= int(input())
 
-    Crear un sistema de descuentos para una tienda. Leer mediante input():
-    1. El precio unitario de un producto (decimal)
-    2. La cantidad de unidades a comprar (entero)
-
-    Calcular el total aplicando los siguientes descuentos según la cantidad:
-    - Si compra 10 o más unidades: 20% de descuento
-    - Si compra entre 5 y 9 unidades: 10% de descuento
-    - Si compra menos de 5 unidades: sin descuento
-
-    Imprimir:
-    1. El subtotal (precio × cantidad)
-    2. El porcentaje de descuento aplicado
-    3. El monto del descuento
-    4. El total final
-
-    Ejemplo:
-        Para las entradas "100" y "12", la salida esperada es:
-        Subtotal: 1200.0
-        Descuento aplicado: 20%
-        Monto de descuento: 240.0
-        Total final: 960.0
-    """
-    pass
+    if cant_unidades >= 10:
+        subtotal= (precio_unitario*cant_unidades)
+        descuento= subtotal*0,2
+        total= subtotal-descuento
+        print(f"Subtotal: {subtotal}")
+        print(f"Descuento: 20%")
+        print(f"Monto de descuento: {descuento}")
+        print(f"Total final: {total}")
+    elif cant_unidades <=9 or cant_unidades>=5:
+        subtotal= (precio_unitario*cant_unidades)
+        descuento= subtotal*0,1
+        total= subtotal-descuento
+        print(f"Subtotal: {subtotal}")
+        print(f"Descuento: 10%")
+        print(f"Monto de descuento: {descuento}")
+        print(f"Total final: {total}")
+    else:
+        subtotal= (precio_unitario*cant_unidades)
+        descuento= subtotal*0
+        total= subtotal-descuento
+        print(f"Subtotal: {subtotal}")
+        print(f"Descuento: 0%")
+        print(f"Monto de descuento: {descuento}")
+        print(f"Total final: {total}")
